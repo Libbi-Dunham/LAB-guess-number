e852e588a0770d0b8f307e2a2ffa4b39eab3edd2
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-04 15:13:41 -0700

    added my if/else statements

--- app.js
+++ app.js
@@ -2,6 +2,41 @@ const result = document.getElementById('result');
 const button = document.getElementById('guess');
 const number = document.getElementById('number');
 
+let numGuess = 10
+let result;
+let theRandomNumber = Math.floor(Math.random() * 20) + 1;
+
+button.addEventListener('click', () =>{
+  guessNum--;
+
+  if (userGuess === randomNum) {
+    result = 'You are correct!';
+  } else
+
+  if (userGuess > randNum) {
+    result = 'You are too high!'
+  } else
+
+  if (userGuess === randomNum) {
+    result = 'You are too low!'
+  } else
+
+  console.log(guessNum);
+
+});
+
+
+let theRandomNumber = Math.floor(Math.random() * 20) + 1;
+
+
+
+
+
+
+
+
+
+
 let randomNum = (1 * 20);
 
 button.addEventListener('click', () =>{
@@ -13,5 +48,3 @@ button.addEventListener('click', () =>{
 )
 
 let theRandomNumber = Math.floor(Math.random() * 20) + 1;
-
-
