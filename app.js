54e139ce8bc3f4bd5ccbe8c90e0b0d181283a175
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-04 17:01:16 -0700

    fixed errors and got my messages/remaining guesses working

--- app.js
+++ app.js
@@ -1,29 +1,38 @@
 const result = document.getElementById('result');
 const button = document.getElementById('guess');
 const number = document.getElementById('number');
+const remainingGuessElement = document.getElementById('remaining-text');
 
-let guessNum = 10
+// let guessNum = 10;
+let remainingGuess = 4;
 let theRandomNumber = Math.floor(Math.random() * 20) + 1;
 
 button.addEventListener('click', () => {
-  guessNum--;
-  console.log(guessNum);
+  console.log('remainging guesses',remainingGuess);
+  // guessNum--;
+  remainingGuess--;
+  // console.log(guessNum);
   let userGuess = Number(number.value);
   console.log(userGuess);
 
   if (userGuess === theRandomNumber) {
-    result = 'You are correct!';
+    result.textContent = 'You are correct!';
   } else
 
   if (userGuess > theRandomNumber) {
-    result = 'You are too high!'
+    result.textContent = 'You are too high!';
   } else
 
-  if (userGuess === theRandomNumber) {
-    result = 'You are too low!'
-  } else
+  if (userGuess < theRandomNumber) {
+    result.textContent = 'You are too low!';
+  }
 
-  console.log(guessNum);
+  // console.log(guessNum);
+
+  const resultText = 'You are ${guess}.';
+  number.textContent = number;
+
+  remainingGuessElement.textContent = remainingGuess
 
 });
 
