eb7287bd2b9be22bc1420074378f8ac6a94482ca
Libbi Dunham <Libbi.Dunham25@gmail.com>
2021-10-04 17:52:58 -0700

    got my reset button to work

--- app.js
+++ app.js
@@ -1,44 +1,52 @@
 const result = document.getElementById('result');
 const button = document.getElementById('guess');
 const number = document.getElementById('number');
+const resetButton = document.getElementById('reset');
+console.log(resetButton);
 const remainingGuessElement = document.getElementById('remaining-text');
 
-// let guessNum = 10;
+
 let remainingGuess = 4;
 let theRandomNumber = Math.floor(Math.random() * 20) + 1;
 
 button.addEventListener('click', () => {
-  console.log('remainging guesses',remainingGuess);
-  // guessNum--;
-  remainingGuess--;
+    console.log('remainging guesses', remainingGuess);
+    remainingGuess--;
+
+    let userGuess = Number(number.value);
+    console.log(userGuess);
+
+    if (userGuess === theRandomNumber) {
+        result.textContent = 'You are correct!';
+    } else
+
+    if (userGuess > theRandomNumber) {
+        result.textContent = 'You are too high!';
+    } else
+
+    if (userGuess < theRandomNumber) {
+        result.textContent = 'You are too low!';
+    }
+
   // console.log(guessNum);
-  let userGuess = Number(number.value);
-  console.log(userGuess);
 
-  if (userGuess === theRandomNumber) {
-    result.textContent = 'You are correct!';
-  } else
+    // result.textContent = `You are ${guess}.`;
+    number.textContent = number;
 
-  if (userGuess > theRandomNumber) {
-    result.textContent = 'You are too high!';
-  } else
-
-  if (userGuess < theRandomNumber) {
-    result.textContent = 'You are too low!';
-  }
-
-  // console.log(guessNum);
-
-  const resultText = 'You are ${guess}.';
-  number.textContent = number;
-
-  remainingGuessElement.textContent = remainingGuess
+    remainingGuessElement.textContent = remainingGuess;
 
 });
 
-reset.addEventListener('click', () => {
-  guessNum = 10;
+resetButton.addEventListener('click', () => {
+    remainingGuess = 4;
+    console.log(remainingGuess);
+    remainingGuessElement.textContent = remainingGuess;
+    number.value = 'Number Here';
+    result.textContent = '';
+    
+    theRandomNumber = Math.floor(Math.random() * 20) + 1;
+
 
 }
-)
+);
 
